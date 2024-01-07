9684eadebb27b28bec00058cd4f87dd4700875a0
gunslinger564 <144602357+gunslinger564@users.noreply.github.com>
2024-01-07 16:05:23 -0600

    Add files via upload

--- RugsExt/media/lua/client/RugRecipes.lua
+++ RugsExt/media/lua/client/RugRecipes.lua
@@ -1,46 +1,53 @@
-rugObjects = rugObjects  or {}
-rugTypes = rugTypes or {}
+
 local function ripCarpet(player,rug)
 		player:getInventory():Remove(rug)
 		player:getInventory():AddItem("Base.Sheet")
 end
 local function convertPiece(player,option,item)
 	player:getInventory():Remove(item)
-	player:getInventory():AddItem(option)
-	
+	getPlayer():getInventory():AddItem(InventoryItemFactory.CreateItem(option))
 end
 
 local function sortChoices(rugName)
-	local choices = {}
-	local r = nil
-		for _, list in pairs(rugTypes) do
-			for i, names in pairs(list) do
-				if string.find(names,rugName) then
-					choices = list
-					r = i
-				end
+	local choices = nil
+		for type,tbl in pairs(RugTypes) do
+			for i,square in pairs(tbl) do
+					for x,t in pairs(MainRugSprites)do
+						if x == type and rugName == square then 	
+							print("found match")	
+							choices = t
+							return choices
+						end
+
+					end
+					if square == rugName then choices = tbl return choices end
+					
 			end
+			
 		end
-	choices[r] = nil
-	return choices
+
+		
 end
 		
 			
 local function rugItemsContextMenuEntry(player, context, items,test)
 	if test and ISInventoryObjectContextMenu.Test then return true end
-  local items = ISInventoryPane.getActualItems(items)
-  for _, item in pairs(items) do
-	for i, name in pairs(rugObjects) do
+	local items = ISInventoryPane.getActualItems(items)
+	
+	for _, item in pairs(items) do
+		for i, name in pairs(rugObjects) do
     		if string.find(item:getFullType(),name) then
-					-- local ripRug = context:addOption(getText("rip rug square"),getSpecificPlayer(player),ripCarpet,item)
-					 local choices = {}
+					local ripRug = context:addOption(getText("rip rug square"),getSpecificPlayer(player),ripCarpet,item)
+					local choices = {}
 					choices = sortChoices(item:getFullType())
-				for _, option in pairs(choices) do
-      				local convertRug = context:addOption(getText("Change rug piece to" .. Translator.getMoveableDisplayName(options)), getSpecificPlayer(player), convertPiece,option,item)
-    			end
+					if choices then
+				for _,option in pairs(choices) do
+      				local convertRug = context:addOption(getText("Change rug piece to" .. option ), getSpecificPlayer(player), convertPiece,option,item)
+				end
+				end
 			end
+		end
 	end
-end
 end
 
 Events.OnFillInventoryObjectContextMenu.Add(rugItemsContextMenuEntry)
